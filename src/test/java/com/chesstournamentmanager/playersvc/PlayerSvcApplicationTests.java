797c23ab9419c4eee76a4241d1bf8f7974a3b3b2
Judah <i460958@fhict.nl>
2022-09-08 00:39:51 +0200

    Added spring boot project

---
+++ src/test/java/com/chesstournamentmanager/playersvc/PlayerSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chesstournamentmanager.playersvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
