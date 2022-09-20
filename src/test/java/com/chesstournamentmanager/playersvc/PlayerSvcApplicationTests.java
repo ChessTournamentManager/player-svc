6df8782569e830f3b34f375dd31a07260a18039a
Judah <i460958@fhict.nl>
2022-09-20 22:40:51 +0200

    Updated CI file, pom and added a test

--- src/test/java/com/chesstournamentmanager/playersvc/PlayerSvcApplicationTests.java
+++ src/test/java/com/chesstournamentmanager/playersvc/PlayerSvcApplicationTests.java
@@ -1,13 +1,22 @@
 package com.chesstournamentmanager.playersvc;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PlayerSvcApplicationTests {
 
+	@Autowired
+	private PlayerSvcApplication playerSvcApplication;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void sendsWelcomeMessage() {
+		assertThat(playerSvcApplication.getMessage()).isEqualTo("Welcome to the player service.");
+	}
 }
